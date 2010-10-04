d2c4ae36cc3aa36ea5f82f6c6851e4f7009a42a3
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-04 17:15:13 -0400

    displays locale messages for all locales

--- Support/spec/show_i18n_message_spec.rb
+++ Support/spec/show_i18n_message_spec.rb
@@ -8,11 +8,29 @@ describe "One en.yml locale file in project" do
   
   it "works for double quotes" do
     line = %Q{  msg = I18n.t("my.project.message") }
-    ShowI18nMessage.display_message(line, @project_dir).should == "This is my project's message"
+    ShowI18nMessage.display_message(line, @project_dir).should == "en: This is my project's message"
   end
 
   it "works for single quotes" do
     line = %Q{  msg = I18n.t('my.project.message') }
-    ShowI18nMessage.display_message(line, @project_dir).should == "This is my project's message"
+    ShowI18nMessage.display_message(line, @project_dir).should == "en: This is my project's message"
+  end
+end
+
+describe "For specific locale" do
+  before do
+    @project_dir = File.expand_path("fixtures/multi_locale", File.dirname(__FILE__))
+  end
+  
+  it "works for double quotes" do
+    line = %Q{  msg = I18n.t("my.project.message") }
+    ShowI18nMessage.display_message(line, @project_dir).should == "en: This is my project's message\nfr: Bonjour!"
+  end
+  
+  it "locale is unmodified" do
+    I18n.locale = :es
+    line = %Q{  msg = I18n.t("my.project.message") }
+    ShowI18nMessage.display_message(line, @project_dir)
+    I18n.locale.should == :es
   end
 end
